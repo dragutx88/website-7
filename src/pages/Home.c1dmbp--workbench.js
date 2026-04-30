56277afd0543820d9d7da349f803d58bfbeb321d
dragutx88 <dragutx88@gmail.com>
2026-05-01 00:00:43 +0300

    Update and rename Home--workbench.js to Home.c1dmbp--workbench.js

---
+++ src/pages/Home.c1dmbp--workbench.js
@@ -0,0 +1,38 @@
+import wixWindow from "wix-window-frontend";
+import wixLocationFrontend from "wix-location-frontend";
+import { session } from "wix-storage-frontend";
+import { searchPlaces } from "backend/liteApi.web";
+import { initSearchForm } from "public/searchForm";
+
+const SEARCH_FLOW_CONTEXT_QUERY_STRINGIFY_SESSION_KEY =
+  "searchFlowContextQueryStringify";
+
+$w.onReady(function () {
+  const renderingEnv = wixWindow.rendering.env;
+
+  if (renderingEnv !== "browser") {
+    console.log("HOME skipped outside browser", { renderingEnv });
+    return;
+  }
+
+  initSearchForm({
+    $w,
+    searchPlacesFn: searchPlaces,
+    debug: false
+  });
+
+  session.setItem(
+    SEARCH_FLOW_CONTEXT_QUERY_STRINGIFY_SESSION_KEY,
+    JSON.stringify({ ...wixLocationFrontend.query, language: "tr", currency: "TRY" })
+  );
+
+  wixLocationFrontend.queryParams.add({
+    ...wixLocationFrontend.query,
+    language: "tr",
+    currency: "TRY"
+  });
+
+  console.log("HOME session/query init", {
+    query: { ...wixLocationFrontend.query, language: "tr", currency: "TRY" }
+  });
+});
