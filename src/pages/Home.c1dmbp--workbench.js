0789fcb62b0d431817ae8b57b7bd0517d1fe7308
dragutx88 <dragutx88@gmail.com>
2026-05-03 21:29:08 +0300

    Merge wixLocationFrontend.query into session data

--- src/pages/Home.c1dmbp--workbench.js
+++ src/pages/Home.c1dmbp--workbench.js
@@ -17,10 +17,10 @@ $w.onReady(function () {
   session.setItem(
     SEARCH_FLOW_CONTEXT_QUERY_STRINGIFY_SESSION_KEY,
     JSON.stringify({
-      ...wixLocationFrontend.query,
       ...JSON.parse(
         session.getItem(SEARCH_FLOW_CONTEXT_QUERY_STRINGIFY_SESSION_KEY) || "{}"
       ),
+      ...wixLocationFrontend.query,
       language: "tr",
       currency: "TRY"
     })
@@ -34,7 +34,9 @@ $w.onReady(function () {
     ...JSON.parse(
       session.getItem(SEARCH_FLOW_CONTEXT_QUERY_STRINGIFY_SESSION_KEY) || "{}"
     ),
-    ...wixLocationFrontend.query
+    ...wixLocationFrontend.query,
+    language: "tr",
+    currency: "TRY"
   };
 
   console.log("HOME session/query init", {
